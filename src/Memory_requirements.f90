9f274dc75b3e7d0a498e76de20f7dd860865254b
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 11:29:14 +0430

    defining variables

--- src/Memory_requirements.f90
+++ src/Memory_requirements.f90
@@ -3,5 +3,12 @@
 ! using the library function `storage_size()`.
 
 program Memory_requirements
-
+    ! defining integer, real, double precision
+    ! complex, character and double complex variables
+    integer :: a
+    real :: b
+    doubleprecision :: c
+    complex :: d
+    character :: e
+    doublecomplex :: f
 end program Memory_requirements
