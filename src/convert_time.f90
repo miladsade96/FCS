6a695b95ed45c052746a9eb97f9f303403a7ad26
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 13:37:33 +0430

    implementing calculation section

--- src/convert_time.f90
+++ src/convert_time.f90
@@ -7,4 +7,10 @@ program convert_time
     integer :: hour, minute, second, temp, input_secs
     print *, "Enter seconds: "
     read *, input_secs
+    second = mod(input_secs, 60)
+    temp = input_secs / 60
+    minute = mod(temp, 60)
+    hour = temp / 60
+    print *, input_secs, "seconds --> ", hour, "hour(s)", &
+            minute, "minute(s)", second, "second(s)."
 end program convert_time
