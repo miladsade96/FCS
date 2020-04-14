174f1cad1a9f934ae97f24f7725ae05a12f81ca4
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 12:27:10 +0430

    Adding comments

--- src/escape_velocity.f90
+++ src/escape_velocity.f90
@@ -1,4 +1,7 @@
-! Created by  on 4/14/20.
+! Created by EverLookNeverSee@GitHub on 4/14/20.
+! This program calculates the escape velocity from earth.
+! The escape velocity is defined as the minimum velocity
+! that a projectile requires to escape from the earth.
 
 program escape_velocity
 
