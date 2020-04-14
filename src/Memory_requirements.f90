4eeff5a2238dd0c971c9e959408166555a867f3d
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 11:26:22 +0430

    Adding comment lines

--- src/Memory_requirements.f90
+++ src/Memory_requirements.f90
@@ -1,4 +1,6 @@
-! Created by  on 4/14/20.
+! Created by EverLookNeverSee@GitHub on 4/14/20.
+! This program shows size of the data types in memory
+! using the library function `storage_size()`.
 
 program Memory_requirements
 
