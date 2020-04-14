26d7516587c7dbee31f9201247404cbb1b0f1a6e
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 12:32:35 +0430

    defining and initializing variable and constants

--- src/escape_velocity.f90
+++ src/escape_velocity.f90
@@ -4,5 +4,10 @@
 ! that a projectile requires to escape from the earth.
 
 program escape_velocity
-
+    implicit none
+    ! radius of earth in meter and mass of earth in kilogram
+    real :: earth_radius = 6378.0e3, earth_mass = 5.98e24
+    ! gravitational constant
+    real :: G = 6.67300e-11
+    real :: escape_velocity
 end program escape_velocity
