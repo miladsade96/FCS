bde472c5a90ffa2baef6e678ecf37babac4b0154
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-14 11:41:50 +0430

    Adding comments

--- src/Mile_to_Km.f90
+++ src/Mile_to_Km.f90
@@ -1,4 +1,6 @@
-! Created by  on 4/14/20.
+! Created by EverLookNeverSee@GitHub  on 4/14/20.
+! This program gets  mile and yard values from user
+! and converts it to kilometer.
 
 program Mile_to_Km
 
