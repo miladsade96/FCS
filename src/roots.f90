26ddf5c2e8b01b36dad30c0fbea4218732e37998
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-15 13:18:44 +0430

    defining variables

--- src/roots.f90
+++ src/roots.f90
@@ -2,5 +2,7 @@
 ! This program calculates the roots of a quadratic equaion.
 
 program roots
-
+    implicit none
+    ! defining variables
+    real :: a, b, c, x_1, x_2, delta, sr_delta
 end program roots
