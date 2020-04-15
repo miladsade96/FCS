d415b51b491cb5f41d578ea1572a833b3fca7a76
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-15 12:27:32 +0430

    Adding comparison section

--- src/max_min.f90
+++ src/max_min.f90
@@ -9,4 +9,11 @@ program max_min
     print *, "Enter two integer number: "
     ! getting user input
     read *, number_1, number_2
+    if (number_1 > number_2) then
+        print *, "maximum:", number_1, "minimum:", number_2
+    elseif (number_2 > number_1) then
+        print *, "maximum:", number_2, "minimum:", number_1
+    else
+        print *, number_1, "and", number_2, "is equal."
+    end if
 end program max_min
