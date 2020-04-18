27427ea40180a44227cdb2906084844898a0d906
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-18 12:57:16 +0430

    Adding select block and case statements

--- src/mark_to_grade.f90
+++ src/mark_to_grade.f90
@@ -7,4 +7,18 @@ program mark_to_grade
     integer :: mark     ! declaring a variable for user input
     print *, "Enter a positive integer value(0 - 100):"
     read *, mark
+    select case(mark)
+        case(0:60)
+            print *, "Repeat", mark
+        case(61:70)
+            print *, "Fair", mark
+        case(71:80)
+            print *, "Good", mark
+        case(81:90)
+            print *, "Very good", mark
+        case(91:100)
+            print *, "Excellent", mark
+        case default
+            print *, "Invalid input value:", mark
+    end select
 end program mark_to_grade
