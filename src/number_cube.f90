673ef943f838f1b4251411f0fd027ec2935a1b3b
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-22 19:16:46 +0430

    Adding comments

--- src/number_cube.f90
+++ src/number_cube.f90
@@ -1,4 +1,7 @@
-! Created by  on 4/22/20.
+! Created by EverLookNeverSee@GitHub on 4/22/20.
+! This program finds all numbers between 100 and 999 such that the
+! number is equal to the sum of the cubes of the digits. For Example:
+! 153 = (1 ** 3) + (5 ** 3) + (3 ** 3)
 
 program number_cube
 
