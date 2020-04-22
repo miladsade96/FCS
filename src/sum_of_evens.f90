7447c747693d16cf47c3acedd0bf991646b55548
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-22 20:24:33 +0430

    changing comment

--- src/sum_of_evens.f90
+++ src/sum_of_evens.f90
@@ -1,6 +1,7 @@
 ! Created by EverLookNeverSee@GitHub on 4/22/20.
-! This program add up all even numbers between given interval.
+! This program add up all even numbers between 1 to user input.
 
 program sum_of_evens
 
+    end do
 end program sum_of_evens
