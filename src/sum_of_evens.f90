f83be14e9a7fafef17669b74b4caf9c703ba2047
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-22 20:37:11 +0430

    Adding do loop and printing result out

--- src/sum_of_evens.f90
+++ src/sum_of_evens.f90
@@ -13,4 +13,11 @@ program sum_of_evens
         print *, "Invalid input!"
         goto 1
     end if
+    do i = 1, number
+        if (mod(i, 2) /= 0) then
+            cycle   ! skipp all odd numbers
+        end if
+        sum = sum + i
+    end do
+    print *, "Sum:", sum
 end program sum_of_evens
