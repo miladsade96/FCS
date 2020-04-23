10e508c7309865a43912eb1ad58a609263d09325
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-23 19:35:28 +0430

    Adding comments

--- src/simpson_rule.f90
+++ src/simpson_rule.f90
@@ -1,4 +1,9 @@
-! Created by  on 4/23/20.
+! Created by EverLookNeverSee@GitHub on 4/23/20.
+! This program integrate a function numerically
+! using simpson's rule.
+
+! Simpson's Rule:
+! https://en.wikipedia.org/wiki/Simpson's_rule
 
 program simpson_rule
 
