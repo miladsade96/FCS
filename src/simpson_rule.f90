37d6bf6807e1bff93feca1f7f0f89e319d89824d
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-23 19:50:56 +0430

    implementing the algorithm

--- src/simpson_rule.f90
+++ src/simpson_rule.f90
@@ -11,4 +11,17 @@ program simpson_rule
     integer :: i, n = 100
     ! declaring inerval and other variables
     real :: a = 1.0, b = 4.0, sum = 0.0, h, x
+    h = (b - a) / n
+    sum = sum + (a ** 3 * exp(-a ** 3) + &
+            b ** 3 * exp(-b ** 3) + 4.0 * (b - h) ** 3&
+            * exp(-(b - h) ** 3))
+    x = a + h
+    do i = 2, n-2, 2
+        sum = sum + 4.0 * (x ** 3 * exp(-x ** 3))
+        x = x + h
+        sum = sum + 2.0 * (x ** 3 * exp(-x ** 3))
+        x = x + h
+    end do
+    sum = h / 3.0 * sum
+    print *, "The result of integration is:", sum
 end program simpson_rule
