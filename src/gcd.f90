5a294a84edf8214c342cae27c6bafec3f046355c
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-23 13:31:36 +0430

    Adding gcd algorithm into program

--- src/gcd.f90
+++ src/gcd.f90
@@ -8,4 +8,15 @@ program gcd
     ! gettig user input
     print *, "Enter two positive integers:"
     read *, num_1, num_2
+    temp = mod(num_1, num_2)
+    if (temp == 0) then
+        print *, "The gcd of", num_1, "and", num_2, "is", num_2
+        stop
+    end if
+    do while (temp /= 0)
+        num_1 = num_2
+        num_2 = temp
+        temp = mod(num_1, num_2)
+    end do
+    print *, "The gcd is", num_2
 end program gcd
