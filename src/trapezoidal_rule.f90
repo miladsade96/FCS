70f1127128f6cf0798ce216ae7fbe0c5c56c9f2f
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-04-23 14:12:17 +0430

    implementing the algorithm

--- src/trapezoidal_rule.f90
+++ src/trapezoidal_rule.f90
@@ -11,4 +11,11 @@ program trapezoidal_rule
     ! declaring length of intervals and sum, h, x variables
     real :: sum = 0.0, h, x
     h = (b - a) / n
+    sum = sum + 0.5 * (a ** 3 * exp(-a ** 3) + b ** 3 * exp(-b ** 3))
+    x = a + h
+    do i = 2, 100
+        sum = sum + (x ** 3 * exp(-x ** 3))
+    end do
+    sum = sum * h
+    print *, "the result of integration is:", sum
 end program trapezoidal_rule
