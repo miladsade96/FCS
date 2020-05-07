6ab7d285f9b2da1fb5c59f1e873f221f83ac8c74
ELNS <EverLookNeverSee@ProtonMail.ch>
2020-05-07 16:13:17 +0430

    implementing the algorithm of factorial

--- src/fact.f90
+++ src/fact.f90
@@ -6,4 +6,15 @@
 function fact(n) result(factorial)
     integer :: n, i
     real :: factorial, temp
+    if (n <= 0) then
+        factorial = -1.0    ! error
+        else if(n == 0) then
+        factorial = 1.0     ! factorial of zero
+        else
+        temp = 1.0
+        do i = 2, n
+            temp = temp * i
+        end do
+        factorial = temp
+    end if
 end function fact
