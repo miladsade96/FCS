4aba2020bc19d5b918f2d68e24a20208865aeef2
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-11 13:21:22 +0430

    declaring character_reverse func

--- src/character_reverse.f90
+++ src/character_reverse.f90
@@ -1,2 +1,6 @@
 ! Created By EverLookNeverSee@GitHub on 5/11/20
 ! this function takes an arbitrary sized character and reverse it.
+
+function character_reverse(ch) result(res)
+
+end function character_reverse
