4cb769fe118cecaea9c15a6681e717eb5f968b4a
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-11 13:27:07 +0430

    declaring dummy parameters and inside variables

--- src/character_reverse.f90
+++ src/character_reverse.f90
@@ -2,5 +2,9 @@
 ! this function takes an arbitrary sized character and reverse it.
 
 function character_reverse(ch) result(res)
-
+    ! declaring dummy parameters
+    character (len=*) :: ch, res
+    ! declaring inside variables
+    integer :: i, it, length
+    length = len(ch)
 end function character_reverse
