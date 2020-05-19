644022d3bb58d27b7864f2c6d24e0c8528731f0e
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-19 19:37:26 +0430

    declaring parameters and variables

--- src/recursive_factorial.f90
+++ src/recursive_factorial.f90
@@ -1,6 +1,8 @@
 ! Created by EverLookNeverSee@GitHub on 5/19/20.
 ! This subroutine computes factorial of n using recursion.
 
-subroutine recursive_factorial()
-
+recursive subroutine recursive_factorial(n, fact)
+    ! declaring variables
+    integer :: n        ! input value
+    integer :: fact     ! output value
 end subroutine recursive_factorial
