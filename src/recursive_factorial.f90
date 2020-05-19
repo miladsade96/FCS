458c096cba87079f867fe2e7ccecbb673083d455
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-19 19:40:27 +0430

    implementing exit point and recursive section

--- src/recursive_factorial.f90
+++ src/recursive_factorial.f90
@@ -5,4 +5,11 @@ recursive subroutine recursive_factorial(n, fact)
     ! declaring variables
     integer :: n        ! input value
     integer :: fact     ! output value
+    ! exit point
+    if (n == 1) then
+        fact = 1
+    else
+        call recursive_factorial((n - 1), fact)
+        fact = n * fact
+    end if
 end subroutine recursive_factorial
