c810bb1202654afca4cdee06c31b41a09a5983bf
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-20 12:24:53 +0430

    defining parameters

--- src/constants.f90
+++ src/constants.f90
@@ -2,5 +2,6 @@
 ! module of constants π , e, and the complex constant i
 
 module constants
-
+    real, parameter :: pi = 3.141593, e = 2.718282
+    real, complex :: ci = (0.0, 1.0)
 end module constants
