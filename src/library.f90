02946d96e9c825b0acca5bbaaf51fc38122b4fb5
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-20 12:28:11 +0430

    Adding comments

--- src/library.f90
+++ src/library.f90
@@ -1,4 +1,7 @@
-! Created by  on 5/20/20.
+! Created by EverLookNeverSee@GitHub on 5/20/20.
+
+!Module that contains a function subprogram F(x) = x + x 2 + π ,
+!and a subroutine SWAP that swaps two variables a and b.
 
 module library
 
