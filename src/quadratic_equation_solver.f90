46d06172868613df34899812c3e29d1a9bade392
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-20 17:09:41 +0430

    calculating the discriminant

--- src/quadratic_equation_solver.f90
+++ src/quadratic_equation_solver.f90
@@ -5,6 +5,8 @@
 
 program quadratic_equation_solver
     ! declaring variables
-    real :: a, b, c, two_a, x_1, x_2, sgn_b
+    real :: a, b, c, two_a, x_1, x_2, sgn_b, disc
     complex :: z_1, z_2
+    ! calculating the discriminant
+    disc = b ** 2 - 4.0 * a * c
 end program quadratic_equation_solver
