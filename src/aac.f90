ef27cf44ff878a567693edafc4fff8e738eee65e
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-25 12:57:42 +0430

    declaring variables

--- src/aac.f90
+++ src/aac.f90
@@ -2,5 +2,9 @@
 ! This program sorts a sequence in ascending order.
 
 program aac
-
+    implicit none
+    ! declaring variables
+    integer :: n, i, j
+    real :: temp
+    real, allocatable, dimension(:) :: sequence
 end program aac
