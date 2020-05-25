637a2da73236ccc322f91da82e350b758c1643eb
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-25 23:41:10 +0430

    declaring variables and matrices

--- src/matrix_product.f90
+++ src/matrix_product.f90
@@ -2,5 +2,9 @@
 ! This program computes the product of two given matrices.
 
 program matrix_product
-
+    implicit none
+    ! declaring variables
+    integer :: m, n, o, p, i, j, k
+    ! declaring matrices
+    real, allocatable, dimension(:,:) :: A, B, C
 end program matrix_product
