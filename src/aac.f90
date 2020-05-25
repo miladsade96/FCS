4d1cb5dc6d92b8670c8cf848848aa123c9a138c7
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-25 12:58:53 +0430

    getting user input in order to specify number of elements in sequence

--- src/aac.f90
+++ src/aac.f90
@@ -7,4 +7,11 @@ program aac
     integer :: n, i, j
     real :: temp
     real, allocatable, dimension(:) :: sequence
+    ! getting user input in order to specify number of elements in sequence.
+    n = -1
+    do
+        print *, "Enter the number of elements in the sequence:"
+        read *, n
+        if (n > 0) exit
+    end do
 end program aac
