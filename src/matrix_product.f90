33c878befeb5b949634f50bef7c4083fc489b234
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-25 23:41:42 +0430

    getting shape of matrix A

--- src/matrix_product.f90
+++ src/matrix_product.f90
@@ -7,4 +7,16 @@ program matrix_product
     integer :: m, n, o, p, i, j, k
     ! declaring matrices
     real, allocatable, dimension(:,:) :: A, B, C
+
+    print *, "Matrix A:"
+    do
+        print *, "Enter number of rows and columns for matrix A:"
+        read *, m, n
+        if (m <= 0 .or. n <= 0) then
+            print *, "number of rows and columns should be positive integers"
+            print *, "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+            cycle
+        end if
+        exit
+    end do
 end program matrix_product
