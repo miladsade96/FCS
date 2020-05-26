7f4768b84122bb8e912fcc17c1fe9d9406e45f6c
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-26 23:38:59 +0430

    Adding comments

---
+++ src/slope_of_chord.f90
@@ -0,0 +1,6 @@
+! created by EverLookNeverSee@GitHub on 05/26/20
+! This program computes slope of the chord in given function.
+
+!               y = sqrt(x) ,       if 0 =< x =< 1
+! y = f(x) -->
+!               y = e ** (1 - x) ,  if x > 1
