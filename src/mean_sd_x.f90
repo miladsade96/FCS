3fe36b3de4eaccb676f78fe2f4f9c8877ca29ba6
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-27 10:34:29 +0430

    defining mean subroutine

--- src/mean_sd_x.f90
+++ src/mean_sd_x.f90
@@ -1,2 +1,6 @@
 ! Created by EverLookNeverSee@GitHub on 5/27/20
 ! This Program computes mean and standrd deviation of n experimental sequence.
+
+subroutine mean(n, seq, mean_value)
+
+end subroutine mean
