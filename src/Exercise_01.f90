f549772586df1e2f5a640e8de9a9406dfcfda1f3
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-28 13:33:38 +0430

    Adding comments into program unit

--- src/Exercise_01.f90
+++ src/Exercise_01.f90
@@ -2,5 +2,11 @@
 ! For more information see FCS/img/Exercise_01.png
 
 program main
+    ! I --> current(amps)
+    ! E --> voltage(volts)
+    ! R --> resistance(ohms)
+    ! L --> inductance(henrys)
+    ! C --> capacitance(farads)
+    ! f --> frequency(herts)
     implicit none
 end program main
