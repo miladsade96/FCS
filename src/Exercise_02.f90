b0902aa8fd23217acdc48e3cd71562e4f0ce1530
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-28 23:45:51 +0430

    declaring variables

--- src/Exercise_02.f90
+++ src/Exercise_02.f90
@@ -7,4 +7,6 @@ program main
     ! w --> wieght per unit volume of filled up earth
     ! h --> height of earth fill
     ! phi --> angle of repose of filled material
+    ! declaring variables
+    real :: w = 513.0, h = 3.0, phi = 30.0, P, radian_phi
 end program main
