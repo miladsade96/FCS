f80adb6d432f3ae6bc7c4fb2a00d5b803db98f2c
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-29 17:46:28 +0430

    making variation of theta

--- src/Exercise_03.f90
+++ src/Exercise_03.f90
@@ -15,4 +15,15 @@ program main
     real, dimension(37) :: x, v
     ! initializing variables
     n = 4; r = 4; w = 2000; theta = 360
+    ! making variation of theta
+    do i = 0, theta, 10
+        ! the argument of trigonometric functions must be in radians
+        radian_theta = i * 3.141593 / 180.0
+        ! storing results in two arrays
+        x(j) = r * (1 + n - cos(radian_theta) - sqrt((n ** 2) - (sin(radian_theta)) ** 2))
+        v(j) = (w * r) * (sin(radian_theta) + &
+                sin(2 * radian_theta) / 2 * (sqrt(n ** 2 - sin(radian_theta) ** 2)))
+        ! increasing counter of arrays blocks
+        j = j + 1
+    end do
 end program main
