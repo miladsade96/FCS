70c3fe16d5d567420e8e2da56a631f737592ebd2
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-31 00:47:41 +0430

    declaring variables

--- src/Exercise_06.f90
+++ src/Exercise_06.f90
@@ -4,4 +4,6 @@
 program main
     ! using integer 64-bit to avoid overflow
     use iso_fortran_env, only: int64
+    ! declaring variables
+    integer(int64) :: i, n, s_1 =0, s_2 =0, s_3 = 0, factorial = 1
 end program main
