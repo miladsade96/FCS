3c05974f31f8a955e8536e1eb966a14c32148626
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-31 00:48:11 +0430

    getting user input

--- src/Exercise_06.f90
+++ src/Exercise_06.f90
@@ -6,4 +6,14 @@ program main
     use iso_fortran_env, only: int64
     ! declaring variables
     integer(int64) :: i, n, s_1 =0, s_2 =0, s_3 = 0, factorial = 1
+    ! getting user input
+    do
+        print *,"Enter a positive integer value:"
+        read *, n
+        if (n <= 0) then
+            print *,"You should enter a positive integer!"
+            cycle
+        end if
+        exit
+    end do
 end program main
