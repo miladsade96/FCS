db254fcb6e5e72d738f00d6b2dd84d64ef83af2b
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-30 19:09:28 +0430

    checking R to compute S

--- src/Exercise_04.f90
+++ src/Exercise_04.f90
@@ -7,4 +7,10 @@ program main
     integer :: i = 1
     real :: R = 20
     real , dimension(37) :: S
+    ! checking current value of R to compute corresponding S
+    1   if (R <= 120) then
+        S(i) = 17000 - (0.485 * R ** 2)
+        else
+        S(i) = 18000 / (1 + (R ** 2 / 18000))
+    end if
 end program main
