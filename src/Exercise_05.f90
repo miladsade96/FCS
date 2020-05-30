f27bdd8fb39b41e77d5e6ebd9cc73364d3ac9272
ELNS <EverLookNeverSee@Protonmail.ch>
2020-05-30 22:35:18 +0430

    calculating delta, roots and printing results

--- src/Exercise_05.f90
+++ src/Exercise_05.f90
@@ -10,4 +10,12 @@ program main
     read *, a
     read *, b
     read *, c
+    ! calculating dscriminant
+    delta = b ** 2 - 4 * a * c
+    sqr_delta = csqrt(delta)
+    ! calculating roots
+    z_1 = (-b + sqr_delta) / (2 * a)
+    z_2 = (-b - sqr_delta) / (2 * a)
+    ! printing results
+    print *, "z_1:", z_1, "z_2:", z_2
 end program main
