b2d8cba0e744d83b33fa821b421103df22db8485
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-02 11:54:05 +0430

    implementing do loop to obtain correct answer from series

--- src/Exercise_08_a.f90
+++ src/Exercise_08_a.f90
@@ -6,4 +6,11 @@ program main
     ! declaring and initializing variables
     real :: sum = 0.0, theta = 1.57079, t = 1.0
     integer :: i = 0
+    ! iterating and calculating cos serie till obtaining correct result
+    do
+        sum = sum + t
+        i = i + 1
+        t = t * (-theta * theta) / (2 * i * (2 * i - 1))
+        if (sum + t == sum) exit
+    end do
 end program main
