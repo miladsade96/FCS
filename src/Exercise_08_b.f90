5d02231ff25222284a5286dd12953219b04fe6ca
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-04 12:36:34 +0430

    writing factorial function once again

--- src/Exercise_08_b.f90
+++ src/Exercise_08_b.f90
@@ -1,7 +1,18 @@
 ! Created by EverLookNeverSee@GitHub on 6/3/20
 ! For more information see FCS/img/Exercise_08_b.png
 
-
-program main
-    implicit none
-end program main
+function fact(n) result(factorial)
+    integer :: n, i
+    real :: factorial, temp
+    if (n < 0) then
+        factorial = -1.0    ! error
+        else if(n == 0) then
+        factorial = 1.0     ! factorial of zero
+        else
+        temp = 1.0
+        do i = 2, n
+            temp = temp * i
+        end do
+        factorial = temp
+    end if
+end function fact
