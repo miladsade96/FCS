597a4b53fe07508c6bdcf1ef7a2dec497899634e
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-04 18:37:47 +0430

    building parallel region and printing thread numbers

--- src/test_open_mp.f90
+++ src/test_open_mp.f90
@@ -6,4 +6,8 @@ program main
     implicit none
     ! setting number of threads
     call omp_set_num_threads(4)
+    print *, "Entering to parallel region"
+    !$omp parallel
+    print *, "Inside paralell region, Thread no:", omp_get_thread_num()
+    !$omp end parallel
 end program main
