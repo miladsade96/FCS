abb902f4846e054afa6a8f3302939774329bdf44
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-06 23:40:11 +0430

    Adding solution comments

--- src/numerical methods/Exercise_08_c.f90
+++ src/numerical methods/Exercise_08_c.f90
@@ -1,2 +1,12 @@
 ! Created by EverLookNeverSee@GitHub on 6/5/20
-! For more information see FCS/img/Exercise_08_d.png
+! For more information see FCS/img/Exercise_08_c.png
+
+! Solution:
+!   Since x = z * (2 ** m) Hence z = x / (2 ** m)
+!   We can choose any `m` as long as -1 <= z <= 1
+!   Our x = 3 , for which m = 2 and z = 0.75 works.
+!   Smaller `m` will give a larger `z` which will give
+!   smaller `xi` which will lead to fewer terms necessary
+!   for a certain precision.
+
+! I could not have done it without @taless474, @diehlpk and @LKedward
