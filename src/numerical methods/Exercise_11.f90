9ec37aa6f1bfb7a81b049564197189c30fd9b0d8
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-10 21:59:27 +0430

    initializing variables

--- src/numerical methods/Exercise_11.f90
+++ src/numerical methods/Exercise_11.f90
@@ -6,4 +6,13 @@ program main
     ! declaring variables
     integer :: i, j, k , root
     logical(1), dimension(1000) :: a
+    ! initializing variables
+    ! setting .true. for all elements of array
+    a = .true.
+    ! assigning false to first element of array(1 is not prime)
+    a(1) = .false.
+    ! calculating square root of size of array
+    ! because we don't need to check all elements of array
+    ! if i^2 > size of array, there is no non-prime number in array.
+    root = int(sqrt(real(size(a))))
 end program main
