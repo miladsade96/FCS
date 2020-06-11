b6af39968772af3ecd15758b6824a9c72f02deb8
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-11 11:15:31 +0430

    getting length of sets from user input

--- src/numerical methods/Exercise_13.f90
+++ src/numerical methods/Exercise_13.f90
@@ -9,4 +9,15 @@ program main
     real, allocatable, dimension(:) :: x, y
     ! initializing temporary variables
     tmp_1 = 0.0; tmp_2 = 0.0; tmp_3 = 0.0; tmp_4 = 0.0; tmp_5 = 0.0
+    ! specifying number of elements in sets
+    do
+        print *, "Enter the number of elements in sets:"
+        read *, n
+        if (n <= 0) then
+            print *, "You should enter a positive integer number!"
+            cycle
+        else
+            exit
+        end if
+    end do
 end program main
