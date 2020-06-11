4b09b7844317291793fa6a525e5f1c608028520a
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-11 11:14:38 +0430

    initializing temporary variables

--- src/numerical methods/Exercise_13.f90
+++ src/numerical methods/Exercise_13.f90
@@ -7,4 +7,6 @@ program main
     integer :: n, i, j
     real :: tmp_1, tmp_2, tmp_3, tmp_4, tmp_5, corr, numerator, denominator
     real, allocatable, dimension(:) :: x, y
+    ! initializing temporary variables
+    tmp_1 = 0.0; tmp_2 = 0.0; tmp_3 = 0.0; tmp_4 = 0.0; tmp_5 = 0.0
 end program main
