8f45908c36971b7328e29ead2003fccae4fb9a06
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-11 10:12:32 +0430

    declaring and initializing variables

--- src/numerical methods/Exercise_12.f90
+++ src/numerical methods/Exercise_12.f90
@@ -1,2 +1,11 @@
 ! Created by EverLookNeverSee@GitHub on 6/11/20
 ! For more information see FCS/img/Exercise_12.png
+
+program main
+    implicit none
+    ! declaring and initializing variables
+    integer :: i
+    real :: tmp
+    real, dimension(50) :: y
+    y = [(i, i = 1, 50)]
+end program main
