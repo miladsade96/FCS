c03d407e92b46942c32d4b409e60c9aa71ab123e
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-13 13:05:18 +0430

    Adding descriptions to variables

--- src/numerical methods/Exercise_14.f90
+++ src/numerical methods/Exercise_14.f90
@@ -3,4 +3,11 @@
 
 program main
     implicit none
+    ! k = 1, 2, 3 for 3 real roots of equation
+    ! a_1, a_2, a_3 --> real coefficients of equation
+    ! x_1 --> real root of equation
+    ! z_2, z_3 --> complex roots of equation
+    ! z_k --> an array to store 3 real roots of equation for k = 1, 2, 3
+    ! discriminant --> to specifying whether the equation has complex roots or not
+    ! theta, omega, Q, R, s --> Check out /img/Exercise_14.png
 end program main
