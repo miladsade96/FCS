61f76cc78a8e70565c72c73c0a55b30ac52f4dd4
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-18 13:23:07 +0430

    defining m_cardano module

--- src/numerical methods/Exercise_14.f90
+++ src/numerical methods/Exercise_14.f90
@@ -1,6 +1,10 @@
 ! Created by EverLookNeverSee@GitHub on 6/12/20
 ! For more information see FCS/img/Exercise_14_Alternative.png
 
+module m_cardano
+    implicit none
+end module m_cardano
+
 program main
     implicit none
 end program main
