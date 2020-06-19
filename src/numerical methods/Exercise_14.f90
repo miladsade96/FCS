532cbbe08051d4edb2ec9e3666eba894fa2c99d4
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-19 12:50:35 +0430

    Adding a comment

--- src/numerical methods/Exercise_14.f90
+++ src/numerical methods/Exercise_14.f90
@@ -1,5 +1,6 @@
 ! Created by EverLookNeverSee@GitHub on 6/12/20
 ! For more information see FCS/img/Exercise_14_Alternative.png
+! I could not have done it without `Stavros Meskos`
 
 module m_cardano
     implicit none
