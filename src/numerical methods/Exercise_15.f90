90f2b88227db0dda12af55c78c147a5fa59ab9cd
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-20 12:26:59 +0430

    defining pi continued fraction expansion

--- src/numerical methods/Exercise_15.f90
+++ src/numerical methods/Exercise_15.f90
@@ -7,4 +7,7 @@ module continued_fractions
     private     ! makes this module private
     ! makes these entities accessible from outside of the module
     public cf, pi_seq
+    ! Simple continued fraction expansion of Pi taken from https://oeis.org/A001203
+    integer, parameter :: pi_seq(20) = [3, 7, 15, 1, 292, 1, 1, 1, 2, 1, 3, 1, &
+                                        14, 2, 1, 1, 2, 2, 2, 2]
 end module continued_fractions
