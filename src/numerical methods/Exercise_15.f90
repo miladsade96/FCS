736093641fb42a771cd38096d9aec929332c1a10
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-20 12:38:30 +0430

    defining cf recursive function

--- src/numerical methods/Exercise_15.f90
+++ src/numerical methods/Exercise_15.f90
@@ -10,4 +10,8 @@ module continued_fractions
     ! Simple continued fraction expansion of Pi taken from https://oeis.org/A001203
     integer, parameter :: pi_seq(20) = [3, 7, 15, 1, 292, 1, 1, 1, 2, 1, 3, 1, &
                                         14, 2, 1, 1, 2, 2, 2, 2]
+    contains
+        recursive function cf(b) result(pq)
+            ! Returns the *simple* continued fraction of the sequence b
+        end function cf
 end module continued_fractions
