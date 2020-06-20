c5771d8ee25e80d7f092f8a253de587425acaf42
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-20 12:44:11 +0430

    implementing conditions

--- src/numerical methods/Exercise_15.f90
+++ src/numerical methods/Exercise_15.f90
@@ -19,5 +19,18 @@ module continued_fractions
             real :: pq_n1(2), pq_n2(2)
             integer :: n
             n = size(b)
+            ! conditions to satisfy `Euler-Wallis` recurrence relations
+            if (n > 2) then
+                pq_n2 = cf(b(1:n-2))
+                pq_n1 = cf(b(1:n-1))
+                pq = b(n)*pq_n1 + pq_n2
+            else if (n == 2) then
+                pq_n1 = cf(b(1:1))
+                pq(1) = b(2)*pq_n1(1) + 1.0
+                pq(2) = b(2)
+            else if (n == 1) then
+                pq(1) = b(1)
+                pq(2) = 1.0
+            end if
         end function cf
 end module continued_fractions
