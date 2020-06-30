779926c4194d2500b03c7df35e74aa39c0f4e8fe
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-30 23:09:16 +0430

    declaring qs subroutine inside interface

--- src/other/quick_sort.f90
+++ src/other/quick_sort.f90
@@ -1,5 +1,9 @@
 program quick_sort
     interface
-
+        ! declaring qs subroutine inside interface
+        recursive subroutine qs(seq, first, last)
+            integer, dimension(:), intent(inout) :: seq
+            integer, intent(in) :: first, last
+        end subroutine qs
     end interface
 end program quick_sort
