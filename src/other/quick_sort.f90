940c34fe221e38641a417adaf599f94980ec3d63
ELNS <EverLookNeverSee@Protonmail.ch>
2020-06-30 23:31:16 +0430

    fixing minor naming issue

--- src/other/quick_sort.f90
+++ src/other/quick_sort.f90
@@ -7,7 +7,7 @@ program quick_sort
         end subroutine qs
     end interface
     ! declaring variables
-    integer, allocatable, dimension(:) :: a
+    integer, allocatable, dimension(:) :: seq
     integer :: first, last, n, i
     ! getting length of sequence from user input
     do
@@ -21,22 +21,22 @@ program quick_sort
         end if
     end do
     ! allocating memory space to array
-    allocate(a(n))
+    allocate(seq(n))
     ! getting elements of sequence from user
     print *, "Please enter the elements of sequence:"
     do i = 1, n
         print *, "Element number", i, ":"
-        read *, a(i)
+        read *, seq(i)
     end do
     ! specifying first and last element
     first = 1
     last = n
     ! printing unsorted sequence
-    print *, "a:",a
+    print *, "a:",seq
     ! calling quick sort subroutine
-    call qs(a, first, last)
+    call qs(seq, first, last)
     ! printing sorted sequence
-    print *, "a:", a
+    print *, "a:", seq
 end program quick_sort
 
 ! defining and implementing qs subroutine
@@ -46,7 +46,7 @@ recursive subroutine qs(seq, first, last)
     integer, intent(in) :: first, last
     integer :: mid, high, low, tmp
     ! initializing local variables
-    mid = a((first + last) / 2)
+    mid = seq((first + last) / 2)
     high = last
     low = first
 end subroutine qs
