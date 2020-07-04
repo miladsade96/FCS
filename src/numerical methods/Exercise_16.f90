9bb0434bd0873dc2bbd582c525c29b46995712f9
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-04 12:53:43 +0430

    Adding initial comments and description

---
+++ src/numerical methods/Exercise_16.f90
@@ -0,0 +1,10 @@
+! Program    : Exercise_16
+! Author     : FortranFan
+! Reference  : https://en.wikipedia.org/wiki/Gauss%27s_continued_fraction#cite_note-8
+!
+! Description:
+! An example implementation that illustrates how to employ a
+! RECURIVE function in Fortran to compute the tangent of x using
+! Lambert's continued fraction dating back to 1768 which gives
+! tan(x) = x/(1-x**2/(3-x**2/(5-x**2/(7-x**2/..))))
+!
