9ca9df3a8b11d0561a59d8dd714b7fdd5fe4f42f
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-12 23:38:21 +0430

    Testing adder interface

--- src/other/test_arithmetic_mod.f90
+++ src/other/test_arithmetic_mod.f90
@@ -6,4 +6,12 @@ program test
     integer :: i_num_1 = 2, i_num_2 = 3
     real :: r_num_1 = 25.0, r_num_2 = 35.0
     complex :: com_num_1 = (7, 8), com_num_2 = (5, -7)
+
+
+    ! Testing adder interface
+    print "(a21)", "Test adder interface:"
+    print "(a8,i1)", "2 + 3 = ", adder(i_num_1, i_num_2)
+    print "(a14,f4.1)", "25.0 + 35.0 = ", adder(r_num_1, r_num_2)
+    print "(a19,(f6.3,f6.3),(f6.3, f6.3))", "(7, 8) + (5, -7) = ", &
+    adder(com_num_1, com_num_2)
 end program test
