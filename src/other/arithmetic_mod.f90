37e065a1b5ea285c4090f54f9a00da034359c042
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-12 23:11:10 +0430

    defining multiplier and divider interfaces

--- src/other/arithmetic_mod.f90
+++ src/other/arithmetic_mod.f90
@@ -11,4 +11,12 @@ module arithmetic_mod
         procedure int_sub, real_sub, complex_sub
     end interface subtractor
 
+    interface multiplier
+        procedure int_mult, real_mult, complex_mult
+    end interface multiplier
+
+    interface divider
+        procedure int_div, real_div, complex_div
+    end interface divider
+
 end module arithmetic_mod
