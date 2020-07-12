5fc8c4b05a30b805b438b5aa018e5ec4e08804e0
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-12 23:37:35 +0430

    declaring and initializing variables

--- src/other/test_arithmetic_mod.f90
+++ src/other/test_arithmetic_mod.f90
@@ -1,4 +1,9 @@
 program test
     use arithmetic_mod  ! importing arithmetic module
     implicit none
+
+    ! declaring and initializing variables
+    integer :: i_num_1 = 2, i_num_2 = 3
+    real :: r_num_1 = 25.0, r_num_2 = 35.0
+    complex :: com_num_1 = (7, 8), com_num_2 = (5, -7)
 end program test
