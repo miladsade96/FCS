1edd08026b09e3a15d45cbeecf62f20a119f54fa
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-12 23:10:33 +0430

    defining subtractor interface

--- src/other/arithmetic_mod.f90
+++ src/other/arithmetic_mod.f90
@@ -6,4 +6,9 @@ module arithmetic_mod
     interface adder
         procedure int_add, real_add, complex_add
     end interface adder
+
+    interface subtractor
+        procedure int_sub, real_sub, complex_sub
+    end interface subtractor
+
 end module arithmetic_mod
