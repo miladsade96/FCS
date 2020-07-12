c619714a15065a38cf7eedb5b7bcd5835bf85050
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-12 23:40:17 +0430

    testing divider interface

--- src/other/test_arithmetic_mod.f90
+++ src/other/test_arithmetic_mod.f90
@@ -28,4 +28,18 @@ program test
     print "(a14,f5.1)", "25.0 * 35.0 = ", multiplier(r_num_1, r_num_2)
     print "(a19,(f6.3,f7.3))", "(7, 8) * (5, -7) = ", &
     multiplier(com_num_1, com_num_2)
+
+    ! Testing divider interface
+    print "(/,a23)", "Test divider interface:"
+    print "(a8,i2)", "2 / 3 = ", divider(i_num_1, i_num_2)
+    print "(a14,f8.6)", "25.0 / 35.0 = ", divider(r_num_1, r_num_2)
+    print "(a19,(f9.6,f9.6))", "(7, 8) / (5, -7) = ", &
+    divider(com_num_1, com_num_2)
+    ! devided by zero tests
+    print "(/,a22)", "Divided by zero tests:"
+    print "(a8,i1)", "2 / 0 = ", divider(i_num_1, 0)
+    print "(a13,f8.6)", "25.0 / 0.0 = ", divider(r_num_1, 0.0)
+    print "(a18,(f9.6,f9.6))", "(7, 8) / (0, 0) = ", &
+    divider(com_num_1, (0.0, 0.0))
+
 end program test
