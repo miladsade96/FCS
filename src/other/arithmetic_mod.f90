d4baf6e06b98af613afa890e1eb3294d575acf53
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-12 23:09:51 +0430

    defining adder interface

--- src/other/arithmetic_mod.f90
+++ src/other/arithmetic_mod.f90
@@ -3,4 +3,7 @@ module arithmetic_mod
     private
     public :: adder, subtractor, multiplier, divider
 
+    interface adder
+        procedure int_add, real_add, complex_add
+    end interface adder
 end module arithmetic_mod
