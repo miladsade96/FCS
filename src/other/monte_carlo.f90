41a5d054022a289de216376991906d10ac833ead
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-14 13:08:27 +0430

    defining and implementing estimator function

--- src/other/monte_carlo.f90
+++ src/other/monte_carlo.f90
@@ -7,4 +7,26 @@ program mc
         print "(a19,i10,7x,a14,f9.7)", "Number of samples: ", n, "Estimated PI: ", pi(n)
         n = n * 10
     end do
+
+contains
+    ! pi value estimator function
+    function pi(samples_num) result(pi_value)
+        ! declaring input parameter
+        integer, intent(in) :: samples_num
+        ! declaring local vaariables
+        real :: pi_value, length
+        real, dimension(2) :: coordinates
+        integer :: i, in_circle
+
+        in_circle = 0
+        do i = 1, samples_num
+            call random_number(coordinates)
+            coordinates = coordinates * 2 - 1
+            length = sqrt(coordinates(1) * coordinates(1) + coordinates(2) * coordinates(2))
+            if ( length <= 1 ) then
+                in_circle = in_circle + 1
+            end if
+        end do
+        pi_value = 4.0  * real(in_circle) / real(samples_num)
+    end function pi
 end program mc
