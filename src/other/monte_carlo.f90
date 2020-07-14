4d73207510ca1d2f942f3120549f72a1afe46a5e
ELNS <EverLookNeverSee@Protonmail.ch>
2020-07-14 13:04:18 +0430

    calling pi function until raaching upper bound

--- src/other/monte_carlo.f90
+++ src/other/monte_carlo.f90
@@ -2,4 +2,9 @@ program mc
     implicit none
     ! declaring and initializing variables
     integer :: n = 1000 ! initial number of samples
+    ! calling pi function until raaching upper bound
+    do while(n <= 1000000000)
+        print "(a19,i10,7x,a14,f9.7)", "Number of samples: ", n, "Estimated PI: ", pi(n)
+        n = n * 10
+    end do
 end program mc
